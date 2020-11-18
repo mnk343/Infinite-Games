e86c6094bb7c79e6c466373c514af43138de3f66
Mayank Wadhwani <mayank343@gmail.com>
2020-11-19 00:13:35 +0530

    Example added

--- safety_games/admissible_strategies.py
+++ safety_games/admissible_strategies.py
@@ -1,4 +1,5 @@
 from copy import copy
+from check_for_winning_strategy import *
 
 # vertices_graph = {0 ,1,2,3,4,5,6,7,8}
 # edges_graph = { 0: [1,3] ,1 : [0,2] , 
@@ -7,20 +8,43 @@ from copy import copy
 # map_vertex_to_player = { 0: 1 ,1:0 , 2:1 , 3: 0 , 4: 1 , 5:0 , 6: 1 , 7:0 ,8:0 }
 
 if __name__ == "__main__":
+    # vertices_graph = {0 ,1,2,3,4,5,6,7,8}
+    # edges_graph = { 0: [1,3] ,1 : [0,2] , 
+    #               2: [1,5] , 3: [4,6] , 4: [0,7,8] , 5:[1,7] , 
+    #               6:[7] , 7: [8,6] , 8:[5]  }
+    # map_vertex_to_player = { 0: 1 ,1:0 , 2:1 , 3: 0 , 4: 1 , 5:1 , 6: 1 , 7:0 ,8:0 }
+
+    # vertices_graph = {0,1,2,3,4}
+    # edges_graph = {
+    #                 0: [1,4],
+    #                 1: [0,2],
+    #                 2: [1,3],
+    #                 3: [3],
+    #                 4: [2]
+    # }
+
+    # # map_vertex_to_player = { 0:0 , 1:1, 2:0 , 3:1 , 4: 1}
+    # map_vertex_to_player = { 0:0 , 1:0, 2:0 , 3:0 , 4: 0}
+
+    # bad_states = [
+    #     {3},
+    #     {0}
+    # ]
     vertices_graph = {0,1,2,3,4}
     edges_graph = {
-                    0: [1,4],
-                    1: [0,2],
-                    2: [1,3],
-                    3: [3],
-                    4: [2]
+                    0: [1],
+                    1: [3,2],
+                    2: [0],
+                    3: [4],
+                    4: [4,1,2,3]
     }
 
-    map_vertex_to_player = { 0:0 , 1:1, 2:0 , 3:1 , 4: 1}
+    # map_vertex_to_player = { 0:0 , 1:1, 2:0 , 3:1 , 4: 1}
+    map_vertex_to_player = { 0:0 , 1:1, 2:0 , 3:0, 4:1}
 
     bad_states = [
-        {2},
-        {3}
+        {3},
+        {4}
     ]
 
     # for i in range(0,5):
@@ -32,26 +56,36 @@ if __name__ == "__main__":
     players = [0, 1]
     edges_graph_with_removed_transitions = copy(edges_graph)
 
-    # player_index = 0
-    # vertex = 1
+    player_index = 0
+    vertex = 0
     # print("mayansk")
     # print(check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index))
+    # for vertex in range(0,5):
+        # print("2")
+    # print(check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index))
     # print(check_if_always_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index))
-
+    value = {}
+    # for vertex in range(0,2):
+    #     for player_index in players:
+    #         if check_if_always_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index) == True:
+    #             value[player_index, vertex] = 1
+    #         elif check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index) == True:
+    #             value[player_index, vertex] = -1
+    #         else:
+    #             value[player_index, vertex] = 0
     while True:
         value = {}
+        # print(check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], 2, 0))
         for vertex in vertices_graph:
             for player_index in players:
-                if not check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index) == True:
-                    value[player_index, vertex] = 0
+                if check_if_always_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index) == True:
+                    value[player_index, vertex] = 1
                 elif check_if_never_winning_stratergy(vertices_graph, edges_graph_with_removed_transitions, map_vertex_to_player, bad_states[player_index], vertex, player_index) == True:
                     value[player_index, vertex] = -1
                 else:
-                    value[player_index, vertex] = 1
-        
-
+                    value[player_index, vertex] = 0
         transitions_old_count = len(all_transitions)
-
+        print(value)
         for player_index in players:
             for vertex, adjacent_vertices in edges_graph.items():
                 if vertex in map_vertex_to_player and map_vertex_to_player[vertex] == player_index:
@@ -63,7 +97,7 @@ if __name__ == "__main__":
                                     edges_graph_with_removed_transitions[vertex].remove(adj_vertex)
 
         transitions_new_count = len(all_transitions)
-
+        print(all_transitions)
         if transitions_new_count == transitions_old_count:
             break
 
--- safety_games/check_for_winning_strategy.py
+++ safety_games/check_for_winning_strategy.py
@@ -1,8 +1,9 @@
+from copy import copy
 # edges_graph is in adjacency matrix format
 
 def find_winning_region_reachability_game(vertices_graph, edges_graph, map_vertex_to_player, good_states):
-	attractor_i = good_states
-
+	attractor_i = copy(good_states)
+	# print("h")
 	updated_length = len(good_states)
 	prev_length = len(good_states)-1
 
@@ -35,7 +36,8 @@ def find_winning_region_reachability_game(vertices_graph, edges_graph, map_verte
 
 	return attractor_i
 
-def check_if_always_winning_stratergy( vertices_graph, edges_graph, map_vertex_to_player, bad_states , initial_state, player_index):
+def check_if_always_winning_stratergy( vertices_graph, edges_graph, vertex_to_player, bad_states , initial_state, player_index):
+	map_vertex_to_player = copy(vertex_to_player)
 	if player_index == 1:
 		for key,value in map_vertex_to_player.items():
 			map_vertex_to_player[key] = (value+1)%2
@@ -47,22 +49,24 @@ def check_if_always_winning_stratergy( vertices_graph, edges_graph, map_vertex_t
 
 	winning_region = find_winning_region_reachability_game(vertices_graph, edges_graph, dual_map_vertex_to_player, good_states)
 	if initial_state not in winning_region: 
+		# print("True")
 		return True
 	return False
 
-def check_if_never_winning_stratergy( vertices_graph, edges_graph, map_vertex_to_player, bad_states , initial_state, player_index):
+def check_if_never_winning_stratergy( vertices_graph, edges_graph, vertex_to_player, bad_states , initial_state, player_index):
+	map_vertex_to_player = copy(vertex_to_player)
 	for key,value in map_vertex_to_player.items():
 		map_vertex_to_player[key] = 0
 
 	dual_map_vertex_to_player = {}
 	good_states = bad_states
-	for vertex in vertices_graph:
-		dual_map_vertex_to_player[vertex] = (map_vertex_to_player[vertex] + 1 ) % 2
+	for node in vertices_graph:
+		dual_map_vertex_to_player[node] = (map_vertex_to_player[node] + 1 ) % 2
 
 	winning_region = find_winning_region_reachability_game(vertices_graph, edges_graph, dual_map_vertex_to_player, good_states)
 	if initial_state not in winning_region: 
-		return True
-	return False
+		return False
+	return True
 
 # vertices_graph = {0 ,1,2,3,4,5,6,7,8}
 # edges_graph = { 0: [1,3] ,1 : [0,2] , 
