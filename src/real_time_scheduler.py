2bd147c9b732f2e99b5db464e5edf0f7f3549f9a
Mayank Wadhwani <mayank343@gmail.com>
2021-04-22 11:04:17 +0530

    Ready for presentation

--- src/real_time_scheduler.py
+++ src/real_time_scheduler.py
@@ -2,26 +2,9 @@ from copy import copy
 import pickle
 from check_for_winning_strategy import *
 
-
 if __name__ == "__main__":
     time_bound = 4
     print(time_bound)
-    # vertices_graph = {1,2,3}
-    # edges_graph = {
-    #                 1: [2,3],
-    #                 2: [1,3],
-    #                 3: [3],
-    # }
-
-    # map_vertex_to_player = { 1:0, 2:1 , 3:0}
-
-    # bad_states = [
-    #     {3},
-    #     {3}
-    # ]
-
-
-    # state = (a1, a2, r1, r2, k1, k2, q1, q2, t1, t2, p)
 
     vertices_graph = set()
     state_index = 1
@@ -237,8 +220,70 @@ if __name__ == "__main__":
         print(map_index_to_state[t[2]])
 
 
-    pickle.dump(good_transitions , open( "good_transitions" + str(time_bound), "wb" ) )
-    pickle.dump(all_transitions , open( "all_transitions" + str(time_bound), "wb" ) )
+
+    time_bound = 4
+    vertices_graph = set()
+    state_index = 1
+    map_index_to_state = {}
+    map_vertex_to_player = {}
+    map_state_to_index = {}
+    edges_graph = {}
+
+    for a1 in range(2):
+        for a2 in range(2):
+            for r1 in range(2):
+                for r2 in range(2):
+                    for k1 in range(time_bound):
+                        for k2 in range(time_bound):
+                            for q1 in range(2):
+                                for q2 in range(2):
+                                    for t1 in range(3):
+                                        for t2 in range(3):
+                                            for p in range(3):
+                                                temp_state = (a1, a2, r1, r2, k1, k2, q1, q2, t1, t2, p)
+                                                vertices_graph.add(state_index)
+                                                map_state_to_index[temp_state] = state_index
+                                                map_index_to_state[state_index] = temp_state
+                                                map_vertex_to_player[ state_index ] = p 
+                                                state_index += 1
+
+    for t in vertices_graph:
+        edges_graph[ t ] = []
+
+    for t in good_transitions:
+        edges_graph[ t[1] ].append( t[2] )
 
 
+    bad_state_index = state_index
+    map_index_to_state[state_index] = (-1)
 
+    vertices_graph.add(bad_state_index)
+    map_vertex_to_player[ state_index ] = 0
+
+    bad_states = [
+        {bad_state_index}, 
+        {bad_state_index}, 
+        {bad_state_index}
+    ]
+
+    start_state = (1,1, 0,0, time_bound-1, time_bound-1, 0, 0 , 2, 2, 0)
+    start_state_index = map_state_to_index[ start_state ]
+    while 1:
+        print(start_state_index)
+        print(map_index_to_state[start_state_index])
+        print( edges_graph[start_state_index] ) 
+        print("")
+
+        if len(edges_graph[start_state_index]) == 0:
+            print("DONE")
+            break
+        if start_state_index == bad_state_index:
+            print("END")
+            break
+
+        start_state = map_index_to_state[start_state_index]
+        if start_state[ 0 ] + start_state[1] + start_state[ 2 ] + start_state[ 3 ] + start_state[ 6 ] + start_state[ 7 ] is 0:
+            print ("ALL PACKETS SENT")
+            break
+
+        start_state_index = edges_graph[start_state_index][0]
